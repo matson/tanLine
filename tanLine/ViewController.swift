62cafa0de5c70faa8d18a3837f15800bd816b6f8
Madison <madison.k.adams@gmail.com>
2023-05-15 13:20:16 -0400

    details

--- tanLine/ViewController.swift
+++ tanLine/ViewController.swift
@@ -8,9 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var weatherImage: UIImageView!
+    
+    @IBOutlet weak var weatherLabel: UILabel!
+   
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+        
+        
         // Do any additional setup after loading the view.
     }
 
