b7786a908a75038b8b7faa6ecfed045677d656e1
Madison <madison.k.adams@gmail.com>
2023-05-23 12:25:05 -0400

    url stuff

--- tanLine/WeatherManager.swift
+++ tanLine/WeatherManager.swift
@@ -7,7 +7,10 @@
 
 import Foundation
 
-//https://api.openweathermap.org/data/2.5/weather?temp=293.39&units=metic&appid=03ac0ea03b5fc4cb1f6cf7d57f66f60c
+/*
+ real key should not be on Github.
+ */
+
 
 struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=03ac0ea03b5fc4cb1f6cf7d57f66f60c&units=metric"
@@ -15,7 +18,26 @@ struct WeatherManager {
     func fetchWeather(cityName: String){
         //by city
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        //print(urlString)
     }
     
+    func performRequest(urlString: String){
+        //1. Create URL
+        if let url = URL(string: urlString){
+            
+            //2. Create a URLSession
+            let session = URLSession(configuration: .default)
+           
+            //3. Give the session a Task:
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+           
+            //4. Start the Task
+            task.resume()
+
+        }
+        
+        
+    }
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+    }
 }
